b84aa39e2688f604d68a9218ee83795e0302bb0f
Divyang Sharma <divyang9575@gmail.com>
2023-08-18 11:07:16 +0530

    n

--- implementation/B_Fancy_Coins.cpp
+++ implementation/B_Fancy_Coins.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
- 
+using namespace std;
 using i64 = long long;
  
 void solve() {
     int m, k, a1, ak;
-    std::cin >> m >> k >> a1 >> ak;
+    cin >> m >> k >> a1 >> ak;
     
-    int t = std::max(0, std::min(m / k, (m - a1 + k - 1) / k));
-    int ans = std::max(0, t - ak) + std::max(0, m - t * k - a1);
+    int t = max(0, min(m / k, (m - a1 + k - 1) / k));
+    int ans = max(0, t - ak) + max(0, m - t * k - a1);
     
     std::cout << ans << "\n";
 }
  
 int main() {
     std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    cin.tie(nullptr);
     
     int t;
-    std::cin >> t;
+    cin >> t;
     
     while (t--) {
         solve();
